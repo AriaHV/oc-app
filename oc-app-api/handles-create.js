f98272b128b89d4e7f6ddb9c47017654f9ed0f4d
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-05-20 12:02:56 +0200

    Added create handles API (forgot to add new files)

---
+++ oc-app-api/handles-create.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+
+export  async function main(event, context, callback) {
+    const data = JSON.parse(event.body);
+    const params = { 
+        TableName: "handles",
+        Item: {
+            handle: data.handle,
+            userId: data.userId,
+            administeredAt: Date.now()
+        }
+    };
+
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false}); 
+    }
+}
