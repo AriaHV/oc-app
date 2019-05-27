d470a7c3b500c7e7251fa0526929ef757c95ce8e
Aria Heijmens Visser <aria.heijmensvisser@gmail.com>
2019-05-27 09:59:55 +0200

    Configured AWS Amplify

---
+++ oc-app-frontend/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "oc-app-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://ntzm62y0ml.execute-api.eu-central-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_9JIqD0TM8",
+      APP_CLIENT_ID: "5pmhojnrq0hhrt1i355s56a7vj",
+      IDENTITY_POOL_ID: "eu-central-1:82d72f62-9dd9-4d67-b41f-9bf6293628fe"
+    }
+  };
+  
